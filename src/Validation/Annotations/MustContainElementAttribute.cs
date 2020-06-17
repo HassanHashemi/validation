0e7b4b3e0d0fe43b58acf6ae4cb1aa2728bd764c
HassanHashemi <HassanHashemi>
2020-06-17 12:19:57 +0430

    add contains element validation

---
+++ src/Validation/Annotations/MustContainElementAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Validation.Annotations
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class MustContainElementAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var propertyType = value.GetType();
+
+            // not a collection, just accept it!
+            if (propertyType.GetInterface(nameof(IEnumerable)) == null)
+            {
+                return true;
+            }
+
+            try
+            {
+                var enumerable = (IEnumerable)value;
+                enumerable.Cast<object>().ContainsElement(nameof(value));
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
